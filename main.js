c54187ca4f22e15ad862fc60b0a85744de5c4303
Diamboy211 <51283163+Diamboy211@users.noreply.github.com>
2020-04-04 21:33:23 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function checkPassword() {
   if (document.getElementById("pass").value === (([]+[][[]])[+[]])+(([]+[][[]])[+[]])+ +([]+[[]])[++[[]][+[]] +[]]) {
-    document.getElementById("password").innerHTML = "How did you find the password? Don't share this password.<br>Also join this discord. Here's the code: exv6pQ9<br>Here's the link: <a href='discord.gg/exv6pQ9'>Here</a>"
+    document.getElementById("password").innerHTML = "How did you find the password? Don't share this password.<br>Also join this discord. Here's the code: exv6pQ9<br>Here's the link: <a href='https://discord.gg/exv6pQ9'>Here</a>"
   }
 }
