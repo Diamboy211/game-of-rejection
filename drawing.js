8f5f3cd22d412a182b6b77a8d93016e967f37089
Diamboy211 <51283163+Diamboy211@users.noreply.github.com>
2020-04-05 12:54:41 +0700

    Update drawing.js

--- drawing.js
+++ drawing.js
@@ -1,4 +1,18 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
-ctx.fillStyle = "#DDDDFF";
-ctx.fillRect(100, 10, 300, 90);
+let a = 0
+
+function updateCanvas() {
+  let b = (Math.sin(a/30)+1)*115
+  a++
+  ctx.fillStyle = "#FFFFFF"
+  ctx.fillRect(0,0,400,100)
+  ctx.fillStyle = "#DDDDFF"
+  ctx.fillRect(b+70,30,100,40)
+  ctx.fillRect(b+30,30,37,8)
+  ctx.fillRect(b,40,67,8)
+  ctx.fillRect(b+20,50,47,8)
+  ctx.fillRect(b+40,60,27,8)
+}
+
+setInterval(updateCanvas, 1000/30)
