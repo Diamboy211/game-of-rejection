80cb0820f2ac5b57eac796956a12eb81cb8078ab
Diamboy211 <51283163+Diamboy211@users.noreply.github.com>
2020-04-05 13:05:04 +0700

    Update main.js

--- main.js
+++ main.js
@@ -7,13 +7,19 @@ var game = {
 }
 
 function checkPassword() {
-  if (document.getElementById("pass").value === (([]+[][[]])[+[]])+(([]+[][[]])[+[]])+ +([]+[[]])[++[[]][+[]] +[]]) {
-    document.getElementById("password").innerHTML = "How did you find the password? Don't share this password.<br>Also join this discord. Here's the code: exv6pQ9<br>Here's the link: <a href='https://discord.gg/exv6pQ9'>Here</a>"
-  } else {
-    game.rejections++
+  if (document.getElementById("pass").value !== "") {
+    if (document.getElementById("pass").value === (([]+[][[]])[+[]])+(([]+[][[]])[+[]])+ +([]+[[]])[++[[]][+[]] +[]]) {
+      document.getElementById("password").innerHTML = "How did you find the password? Don't share this password.<br>Also join this discord. Here's the code: exv6pQ9<br>Here's the link: <a href='https://discord.gg/exv6pQ9'>Here</a>"
+      document.getElementById("canvas").style.display = "none"
+    } else {
+      document.getElementById("pass").value = ""
+      game.rejections++
+   }
   }
 }
 
 function update() {
-  
+  document.getElementById("rejections").textContent = "Rejections: " + game.rejections
 }
+
+setInterval(update, 1000/30)
