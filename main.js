53074ff6d7473a390d45306d5e3405af5ece838f
Diamboy211 <51283163+Diamboy211@users.noreply.github.com>
2020-04-05 11:16:57 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
+var game = {
+  rejections: 0,
+  guessers: {
+    amount: 0,
+    overclockRatio: 0,
+  },
+}
+
 function checkPassword() {
   if (document.getElementById("pass").value === (([]+[][[]])[+[]])+(([]+[][[]])[+[]])+ +([]+[[]])[++[[]][+[]] +[]]) {
     document.getElementById("password").innerHTML = "How did you find the password? Don't share this password.<br>Also join this discord. Here's the code: exv6pQ9<br>Here's the link: <a href='https://discord.gg/exv6pQ9'>Here</a>"
+  } else {
+    game.rejections++
   }
+}
+
+function update() {
+  
 }
